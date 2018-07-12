01df3e76f4d2e442a94a6c0bb31255d1adf289b4
Jason Joyce <fuzzball81@gmail.com>
2018-07-12 14:09:00 -0400

    Initial project setup (#1)
    Adding an initial directory structure as well as support files for:
    
    - CI: .travis.yml
    - Auto pull request merge: .mergify.yml
    - Pipenv: Pipfile, Pipfile.lock
    - Testing; tox.ini
    
    Along with several more.
    
    Signed-off-by: Jason Joyce <fuzzball81@gmail.com>

---
+++ tests/test_atkinson.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `atkinson` package."""
+
+from atkinson import atkinson
+
+
+def test_basic_run():
+    """Basic run test"""
+    atkinson.main()
