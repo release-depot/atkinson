ea1fbe8e6474db11f18464777b6d074b70ac642c
Jason Joyce <fuzzball81@gmail.com>
2018-09-25 17:04:51 -0400

    Rotating config search functions to generators.
    Changing config_search_paths and get_config_files to be generators
    instead of functions that return lists. This allows for better
    performance and less memory usage. The other benefit is a config manager
    can better handle stacked configs where you may have overrides in one
    file but want the defaults from another.
    
    Pulled the default search paths to a global constant to make testing
    easier.
    
    Adding the ability to pass get_config_files a list of file names to
    search for and have found paths returned.
    
    Signed-off-by: Jason Joyce <fuzzball81@gmail.com>

--- atkinson/config/search.py
+++ atkinson/config/search.py
@@ -6,34 +6,63 @@ from __future__ import print_function
 import os
 
 
+# Using a tuple here over a list to insure it's immutable.
+DEFAULT_CONFIG_PATHS = (os.path.realpath('./configs'),
+                        '/etc/atkinson',
+                        os.path.expanduser('~/.atkinson'))
+
+
 def _check_available(filename):   # pragma: no cover
-    """Check to see if the filename exists and is a file"""
+    """
+    Check to see if the filename exists and is a file
+
+    :param filename: str A fully qualified path and file
+    :returns: Boolean
+    """
     return os.path.exists(filename) and os.path.isfile(filename)
 
 
 def config_search_paths(override_list=None):
-    """Generate a list of paths to search for config files"""
-    default = [os.path.expanduser('~/.atkinson'), '/etc/atkinson', os.path.realpath('./configs')]
-    if override_list is None:
-        return default
+    """
+    Generate a list of paths to search for config files
 
-    ret_list = []
-    if isinstance(override_list, list):
-        for raw in override_list:
-            ret_list.append(os.path.realpath(os.path.expanduser(raw)))
-    else:
-        ret_list = [os.path.realpath(os.path.expanduser(override_list))]
+    :param override_list: A list for string path to use as a override location Default: None
+    :returns: generator function of search paths.
+    """
+    for default in DEFAULT_CONFIG_PATHS:
+        yield default
 
-    ret_list.extend(default)
-    return ret_list
+    if override_list is not None:
+        if isinstance(override_list, list):
+            for override_path in override_list:
+                yield os.path.realpath(os.path.expanduser(override_path))
+        else:
+            yield os.path.realpath(os.path.expanduser(override_list))
 
 
-def get_config_file(filename='config.yml', overrides=None):
-    """Search for filename, or return the default config file"""
-    ret_path = ''
+def get_config_files(filenames=None, overrides=None, add_defaults=True):
+    """
+    Search for filename, or return the default config file
+
+    :param filenames: list or string of file names to search for Default: None
+    :param overrides: list of string of paths to use for searching Default: None
+    :param add_defaults: Boolean Control if the default file name (config.yml) is added
+    to the search
+
+    :returns: generator function of available config files
+    """
+    file_names = []
+    if add_defaults is True:
+        file_names.append('config.yml')
+
+    if filenames is not None:
+        if isinstance(filenames, list):
+            file_names.extend(filenames)
+        else:
+            file_names.append(filenames)
+
     for path in config_search_paths(override_list=overrides):
-        full_path = os.path.join(path, filename)
-        if _check_available(full_path):
-            ret_path = full_path
-            break
-    return ret_path
+        for filename in file_names:
+            full_path = os.path.join(path, filename)
+            if _check_available(full_path):
+                yield full_path
