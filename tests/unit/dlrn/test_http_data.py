eba25aac5b0cee2d036114061fe54881f4301531
Jason Joyce <fuzzball81@gmail.com>
2019-01-17 18:26:19 -0500

    Fixing an issue where dlrn extended hashes were not supported.
    When an extended hash is in use, DlrnHttpData did not generate the
    proper URL to fetch data.
    
    Refactored the commit and get_versions methods into properties.
    
    Added a link property to the dlrn config files to set the initial dlrn symlink
    to use. This can been overridden in the constructors.
    
    Signed-off-by: Jason Joyce <fuzzball81@gmail.com>

--- tests/unit/dlrn/test_http_data.py
+++ tests/unit/dlrn/test_http_data.py
@@ -8,7 +8,7 @@ import requests_mock
 from atkinson.dlrn.http_data import DlrnHttpData, dlrn_http_factory
 
 
-def test_factory_good_config(datadir):
+def test_factory_bad_connnection(datadir):
     """
     Given we have a valid config file
     When we call the factory function
@@ -35,31 +35,29 @@ def test_no_data_good_status(datadir):
     GIVEN we have a DlrnHttpData instance with a working config
     WHEN we fetch the commit yaml but it contains no data
     AND the status code is good.
-    THEN we get an empty list back from the call
+    THEN we get an empty dictionary back from the call
     """
     config_file = os.path.join(datadir, 'config.yml')
     with requests_mock.Mocker() as mock:
-        mock.get('http://testhost/test/consistent/commit.yaml',
+        mock.get('http://testhost/test/link_name/commit.yaml',
                  text='',
                  status_code=200)
         dlrn = dlrn_http_factory('tester', config_file=config_file)
-        actual = dlrn.commit('consistent')
-        assert [] == actual
+        assert {} == dlrn.commit
 
 
 def test_no_data_bad_status(datadir):
     """
     GIVEN we have a DlrnHttpData instance with a working config
     WHEN we fetch the commit yaml but get a bad status code
-    THEN we get an empty list back from the call
+    THEN we get an empty dictionary back from the call
     """
     config_file = os.path.join(datadir, 'config.yml')
     with requests_mock.Mocker() as mock:
-        mock.get('http://testhost/test/consistent/commit.yaml',
+        mock.get('http://testhost/test/link_name/commit.yaml',
                  status_code=404)
         dlrn = dlrn_http_factory('tester', config_file=config_file)
-        actual = dlrn.commit('consistent')
-        assert [] == actual
+        assert {} == dlrn.commit
 
 
 def test_good_data_good_status(datadir):
@@ -67,21 +65,21 @@ def test_good_data_good_status(datadir):
     GIVEN we have a DlrnHttpData instance with a working config
     WHEN we fetch the commit yaml and it contains data
     AND the status code is good
-    THEN we get a list of commits back from the call
+    THEN we get a commit data back from the call
     """
     config_file = os.path.join(datadir, 'config.yml')
     real_data = os.path.join(datadir, 'commit.yaml')
-    expected = [{'name': 'python-networking-l2gw',
-                 'dist_hash': '44eea44aaa02e9d314c0288788254a9f316c5772',
-                 'commit_hash': '1c087246c4a31bec1124acde610731c884c435f1'}]
+    expected = {'name': 'python-networking-l2gw',
+                'dist_hash': '44eea44aaa02e9d314c0288788254a9f316c5772',
+                'commit_hash': '1c087246c4a31bec1124acde610731c884c435f1',
+                'extended_hash': None}
     with open(real_data, 'r') as data:
         with requests_mock.Mocker() as mock:
-            mock.get('http://testhost/test/consistent/commit.yaml',
+            mock.get('http://testhost/test/link_name/commit.yaml',
                      text=data.read(),
                      status_code=200)
             dlrn = dlrn_http_factory('tester', config_file=config_file)
-            actual = dlrn.commit('consistent')
-            assert expected == actual
+            assert expected == dlrn.commit
 
 
 def test_good_data_bad_status(datadir):
@@ -94,26 +92,47 @@ def test_good_data_bad_status(datadir):
     config_file = os.path.join(datadir, 'config.yml')
     real_data = os.path.join(datadir, 'commit.yaml')
     with open(real_data, 'r') as data:
-        expected = []
+        expected = {}
         with requests_mock.Mocker() as mock:
-            mock.get('http://testhost/test/consistent/commit.yaml',
+            mock.get('http://testhost/test/link_name/commit.yaml',
                      text=data.read(),
                      status_code=404)
             dlrn = dlrn_http_factory('tester', config_file=config_file)
-            actual = dlrn.commit('consistent')
-            assert expected == actual
+            assert expected == dlrn.commit
+
+
+def test_extended_hash(datadir):
+    """
+    GIVEN we have a DlrnHttpData instance
+    WHEN we have commit data that has an extended hash
+    THEN the commit data contains a hash instead of None
+    """
+    config_file = os.path.join(datadir, 'config.yml')
+    real_data = os.path.join(datadir, 'extend_commit.yaml')
+    expected = {'name': 'test-package',
+                'dist_hash': 'zyx987',
+                'commit_hash': 'abc123',
+                'extended_hash': 'asdf567'}
+    with open(real_data, 'r') as data:
+        with requests_mock.Mocker() as mock:
+            mock.get('http://testhost/test/link_name/commit.yaml',
+                     text=data.read(),
+                     status_code=200)
+            dlrn = dlrn_http_factory('tester', config_file=config_file)
+            assert expected == dlrn.commit
 
 
 def test_get_versions_good_status(datadir):
     """
-    GIVEN we have a DlrnHttpData instance and commit and distgit hashs
-    WHEN we call get_versions
+    GIVEN we have a DlrnHttpData instance
+    WHEN we call versions
     AND the status code is good
     THEN we get a dictionary with data
     """
     config_file = os.path.join(datadir, 'config.yml')
     real_data = os.path.join(datadir, 'versions.csv')
-    with open(real_data, 'r') as data:
+    commit_data = os.path.join(datadir, 'test_commit.yaml')
+    with open(real_data, 'r') as data, open(commit_data, 'r') as commit:
         expected = {'test-project': {'source': 'abc123',
                                      'state': 'SUCCESS',
                                      'distgit': 'zxy987',
@@ -122,26 +141,58 @@ def test_get_versions_good_status(datadir):
             mock.get('http://testhost/test/ab/c1/abc123_zxy987/versions.csv',
                      text=data.read(),
                      status_code=200)
+            mock.get('http://testhost/test/link_name/commit.yaml',
+                     text=commit.read(),
+                     status_code=200)
             dlrn = dlrn_http_factory('tester', config_file=config_file)
-            actual = dlrn.get_versions('abc123', 'zxy987')
-            assert expected == actual
+            assert expected == dlrn.versions
 
 
 def test_get_versions_bad_status(datadir):
     """
-    GIVEN we have a DlrnHttpData instance and commit and distgit hashs
-    WHEN we call get_versions
+    GIVEN we have a DlrnHttpData instance
+    WHEN we call versions
     AND the status code is bad
     THEN we get an empty dictionary
     """
     config_file = os.path.join(datadir, 'config.yml')
     real_data = os.path.join(datadir, 'versions.csv')
-    with open(real_data, 'r') as data:
+    commit_data = os.path.join(datadir, 'test_commit.yaml')
+    with open(real_data, 'r') as data, open(commit_data, 'r') as commit:
         expected = {}
         with requests_mock.Mocker() as mock:
             mock.get('http://testhost/test/ab/c1/abc123_zxy987/versions.csv',
                      text=data.read(),
                      status_code=404)
+            mock.get('http://testhost/test/link_name/commit.yaml',
+                     text=commit.read(),
+                     status_code=200)
             dlrn = dlrn_http_factory('tester', config_file=config_file)
-            actual = dlrn.get_versions('abc123', 'zxy987')
-            assert expected == actual
+            assert expected == dlrn.versions
+
+
+def test_extend_hash_version(datadir):
+    """
+    GIVEN we have a DlrnHttpData instance and commit data with an extended hash
+    WHEN we call versions
+    THEN we get a dictionary with data
+    """
+    config_file = os.path.join(datadir, 'config.yml')
+    real_data = os.path.join(datadir, 'versions.csv')
+    commit_data = os.path.join(datadir, 'extend_commit.yaml')
+    with open(real_data, 'r') as data, open(commit_data, 'r') as commit:
+        expected = {'test-project': {'source': 'abc123',
+                                     'state': 'SUCCESS',
+                                     'distgit': 'zxy987',
+                                     'nvr': 'test-project-1.0.el7'}}
+        with requests_mock.Mocker() as mock:
+            url1 = ('http://testhost/test/ab/c1/abc123_zyx987'
+                    '_asdf567/versions.csv')
+            mock.get(url1,
+                     text=data.read(),
+                     status_code=200)
+            mock.get('http://testhost/test/link_name/commit.yaml',
+                     text=commit.read(),
+                     status_code=200)
+            dlrn = dlrn_http_factory('tester', config_file=config_file)
+            assert expected == dlrn.versions
