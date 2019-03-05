2e558e4b92fe17234fabbadbe869fc161124bc0f
Jason Joyce <fuzzball81@gmail.com>
2019-03-05 18:45:28 -0500

    Doc cleanup for the error reporters.
    A space was missing between the method description and the parameters
    causing the documentation to not render correctly.
    
    Added additional missing documentation for the base class.
    
    Signed-off-by: Jason Joyce <fuzzball81@gmail.com>

--- atkinson/errors/reporters/__init__.py
+++ atkinson/errors/reporters/__init__.py
@@ -12,22 +12,41 @@ class BaseReport(ABC):
     @classmethod
     @abstractmethod
     def new(cls, title, description, config):
-        """ Create a new report and return a report object """
+        """
+        Create a new report and return a report object
+
+        :param title: A title for the report (str)
+        :param description: Description for the report (str)
+        :param config: Configuration information for the report (dict)
+        """
 
     @classmethod
     @abstractmethod
     def get(cls, report_id, config):
-        """ Get a current report object by ID """
+        """
+        Get a current report object by ID
+
+        :param report_id: The ID for the report (str)
+        :param config: Configuration information for the report (dict)
+        """
 
     @property
     @abstractmethod
     def report_id(self):
-        """ The id of the report """
+        """
+        The id of the report
+        """
 
     @abstractmethod
     def update(self, **kwargs):
-        """ Update the data on an active report """
+        """
+        Update the data on an active report
+
+        :param kwargs: Name=value pairs of data to update in the report
+        """
 
     @abstractmethod
     def close(self):
-        """ Close out the report """
+        """
+        Close out the report
+        """
